89c33fe97dafc465b5fd9bedbfe437fed187f7e6
Sushmita <sushmitakumari363@gmail.com>
2023-06-08 19:15:53 +0530

    ReactRouter set up

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { makeServer } from "./server";
+import { BrowserRouter as Router } from "react-router-dom";
 
 makeServer();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
 
