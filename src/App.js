9dcee063659b2eeb63410f8d87868174df3ccd2a
Sushmita <sushmitakumari363@gmail.com>
2023-06-09 10:36:52 +0530

    UserFeed set up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import { UserFeed } from "./Pages/UserFeed/UserFeed";
 
 function App() {
   return (
     <div className="App">
-      <Routes></Routes>
+      <Routes>
+        <Route path="/" element={<UserFeed />} />
+      </Routes>
     </div>
   );
 }
