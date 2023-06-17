7c5a933fb0bdd0efbefb32e88e94ecbc4365e171
Sushmita <sushmitakumari363@gmail.com>
2023-06-17 15:51:19 +0530

    SideBar Css Update

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,7 +1,14 @@
 import IconButton from "@mui/material/IconButton";
 import Card from "@mui/material/Card";
+import BookmarksIcon from "@mui/icons-material/Bookmarks";
+import HomeIcon from "@mui/icons-material/Home";
+import LogoutIcon from "@mui/icons-material/Logout";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import ExploreIcon from "@mui/icons-material/Explore";
+import { useNavigate } from "react-router-dom";
 
 export const SideBar = (props) => {
+  const navigate = useNavigate();
   return (
     <>
       <Card
@@ -10,14 +17,36 @@ export const SideBar = (props) => {
           flexDirection: "column",
           flexWrap: "wrap",
           width: "250px",
+          padding: "5px",
         }}
       >
-        <IconButton>Home</IconButton>
-        <IconButton>Explore</IconButton>
-        <IconButton>Likes</IconButton>
-        <IconButton>Bookmark</IconButton>
-        <IconButton>Logout</IconButton>
-        <IconButton>Post</IconButton>
+        <IconButton onClick={() => navigate("/")}>
+          <HomeIcon />
+          Home
+        </IconButton>
+        <IconButton>
+          <ExploreIcon />
+          Explore
+        </IconButton>
+        <IconButton onClick={() => navigate("/bookmark")}>
+          <BookmarksIcon />
+          Bookmark
+        </IconButton>
+        <IconButton>
+          <LogoutIcon />
+          Logout
+        </IconButton>
+        <IconButton
+          sx={{
+            backgroundColor: "#dabdff",
+            border: "1px solid",
+            pointerEvents: "none",
+            borderRadius: "5px",
+          }}
+        >
+          <AddCircleOutlineIcon />
+          Post
+        </IconButton>
       </Card>
     </>
   );
