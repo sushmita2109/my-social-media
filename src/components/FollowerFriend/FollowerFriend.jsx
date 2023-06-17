45f5e507c3664a1ccbf85d7e3ba5e775d5e326a8
Sushmita <sushmitakumari363@gmail.com>
2023-06-17 17:12:06 +0530

    followerfriend style added

--- src/components/FollowerFriend/FollowerFriend.jsx
+++ src/components/FollowerFriend/FollowerFriend.jsx
@@ -5,6 +5,7 @@ import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import { usePost } from "../../context/PostContext/PostContext";
 import { useAuth } from "../../context/AuthContext/AuthContext";
 import { useEffect } from "react";
+import "./FollowerFriend.css";
 
 export const FollowerFriend = () => {
   const { postDispatch, postStates } = usePost();
@@ -30,19 +31,40 @@ export const FollowerFriend = () => {
   }, []);
   return (
     <div>
-      <Card>
+      <Card sx={{ width: "300px" }}>
         <Typography variant="h6">Suggested Users</Typography>
 
         <div>
           {postStates?.users?.map((user) => (
-            <div key={user._id}>
-              <AccountCircleIcon></AccountCircleIcon>
-              {user.firstName}
-              <div className="username-container">
+            <Card
+              key={user._id}
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                flexWrap: "wrap",
+                justifyContent: "space-between",
+                gap: "8px",
+                padding: "5px",
+              }}
+            >
+              <div className="username-cont">
+                <div className="username">
+                  <AccountCircleIcon></AccountCircleIcon>
+                  {user.firstName}
+                </div>
                 <p>@{user.username}</p>
               </div>
-              <Button>Follow</Button>{" "}
-            </div>
+              <Button
+                sx={{
+                  backgroundColor: "#dabdff",
+                  pointerEvents: "none",
+                  height: "45px",
+                  marginTop: "5px",
+                }}
+              >
+                Follow
+              </Button>
+            </Card>
           ))}
         </div>
       </Card>
