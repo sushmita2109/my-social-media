82b8aa389b8993ee180ecba3adec6c725f9e664a
Sushmita <sushmitakumari363@gmail.com>
2023-06-19 17:41:52 +0530

    create post modal added

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -6,9 +6,27 @@ import LogoutIcon from "@mui/icons-material/Logout";
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 import ExploreIcon from "@mui/icons-material/Explore";
 import { useNavigate } from "react-router-dom";
+import { Modal, Box, Button, Typography } from "@mui/material";
+import { useState } from "react";
+import { CreatePost } from "../CreatePost/CreatePost";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
 export const SideBar = (props) => {
   const navigate = useNavigate();
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
     <>
       <Card
@@ -38,15 +56,24 @@ export const SideBar = (props) => {
         </IconButton>
         <IconButton
           sx={{
-            backgroundColor: "#dabdff",
             border: "1px solid",
-            pointerEvents: "none",
             borderRadius: "5px",
           }}
+          onClick={handleOpen}
         >
           <AddCircleOutlineIcon />
           Post
         </IconButton>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <CreatePost handleClose={handleClose} />
+          </Box>
+        </Modal>
       </Card>
     </>
   );
