e3f60f9cebf792aa9f733a224456c21dec3d2041
Sushmita <sushmitakumari363@gmail.com>
2023-06-23 16:56:09 +0530

    Deleted Post done

--- src/components/PostCard/Postcard.jsx
+++ src/components/PostCard/Postcard.jsx
@@ -1,56 +1,14 @@
 import { usePost } from "../../context/PostContext/PostContext";
+import { ContextualMenuBar } from "../ContextualMenuBar/ContextualMenuBar";
 import Card from "@mui/material/Card";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import "./PostCard.css";
 import { ActionButtons } from "../ActionButtons/ActionButtons";
 import { useNavigate } from "react-router-dom";
 import { CreatePost } from "../CreatePost/CreatePost";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
-import { Button } from "@mui/material";
-import Menu from "@mui/material/Menu";
-import MenuItem from "@mui/material/MenuItem";
-import { useState } from "react";
 
 const ascendingOrder = (a, b) => new Date(b.updatedAt) - new Date(a.updatedAt);
 
-const ContextualMenuBar = ({ post, getDeletedData }) => {
-  const [anchorEl, setAnchorEl] = useState(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = (e, post) => {
-    if (e.target.innerText === "Delete") {
-      getDeletedData(post._id);
-    }
-    setAnchorEl(null);
-  };
-
-  return (
-    <>
-      <Button
-        startIcon={<MoreVertIcon />}
-        id="basic-button"
-        aria-controls={open ? "basic-menu" : undefined}
-        aria-haspopup="true"
-        aria-expanded={open ? "true" : undefined}
-        onClick={handleClick}
-      ></Button>
-      <Menu
-        id="basic-menu"
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        MenuListProps={{
-          "aria-labelledby": "basic-button",
-        }}
-      >
-        <MenuItem onClick={(e) => handleClose(e, post)}>Edit</MenuItem>
-        <MenuItem onClick={(e) => handleClose(e, post)}>Delete</MenuItem>
-      </Menu>
-    </>
-  );
-};
 export const PostCard = () => {
   const { postStates, getDeletedData } = usePost();
 
